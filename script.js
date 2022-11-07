2d31da028e0f9b5adb981d5a729089515702305b
Mustafa Johnny <mustafajohnn99@gmail.com>
2022-11-07 20:49:09 +0300

    updates 2

--- script.js
+++ script.js
@@ -52,3 +52,61 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+// All this logic will be for the user chosen the tariff and then putting his number and send a message to the telegram.
+const modal = document.querySelector(".modal");
+const modal2 = document.querySelector(".modal2");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal2 = document.querySelector(".btnCloseModals");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsProduct = document.querySelectorAll(".choseProductBtn");
+const userPhoneNum = document.querySelector(".PhoneNumInput");
+const btnSendRequst = document.querySelector(".btnSendTele");
+let productName;
+
+// Our Main bot address that we will send a message later..
+const bot = new Bot(
+  "5730249668:AAGvnXi32p-5K-9JZnsEbwwYXRxmSi2v5aY",
+  -809597330
+);
+
+btnsProduct.forEach((ele) =>
+  ele.addEventListener("click", (event) => {
+    productName = event.target.value;
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  })
+);
+
+btnSendRequst.addEventListener("click", (event) => {
+  const userNumber = userPhoneNum.value;
+
+  if (userNumber.trim() === "") return;
+
+  bot.sendMessage(`Tactic Menu : ${productName}  ${userNumber}`).then((res) => {
+    console.log(res);
+  });
+
+  closeModal();
+
+  modal2.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+// Close the modal
+const closeModal = function () {
+  modal.classList.add("hidden");
+  modal2.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Calling the closeModal function on click and also when the user clicks anywhere on the overlay.
+btnCloseModal.addEventListener("click", closeModal);
+
+// Close the second modal
+
+btnCloseModal2.addEventListener("click", () => {
+  modal2.classList.add("hidden");
+  overlay.classList.add("hidden");
+  userPhoneNum.value = "";
+});

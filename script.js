9069b40a3f6825594f34a4ee9849bd618c617992
Mustafa Johnny <mustafajohnn99@gmail.com>
2022-11-24 00:21:17 +0300

    footer bug is fixed

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-// Main function that is responsible for scroling the whole page
-const scroll = new SmoothScroll('a[href*="#"]');
+"use strict";
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+// Simple function we use it when the user clicks on one of navigation links, we scroll smothly to the needed section.
+document
+  .querySelector(".main-nav-list")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Matching strategy so that we can scroll to the needed section.
+    if (
+      e.target.classList.contains("main-nav-link") ||
+      "navLinkDeskT" ||
+      "navLinkPhoneT"
+    ) {
+      headerEl.classList.toggle("nav-open");
+      const id = e.target.getAttribute("href");
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+  });
 
 // Selecting the elements for the tarrifes product (phone version)
 const itemAll = document.querySelectorAll(".item");
@@ -9,48 +29,11 @@ const parentElement = document.querySelector(".accordion");
 parentElement.addEventListener("click", (e) => {
   const clicked = e.target.closest(".item");
   clicked.classList.toggle("open");
-
-  //   itemAll.forEach((ele) => ele.classList.remove("open"));
-  //   e.preventDefault();
 });
-
-// Mobile navigation work and logic
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
 
 // Toggling the open/close button
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
-});
-
-// Smooth scrolling animation For Mobile
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-
-    if (link.classList.contains("navLinkPhoneT"))
-      headerEl.classList.toggle("nav-open");
-  });
 });
 
 // All this logic will be for the user chosen the tariff and then putting his number and send a message to the telegram.
